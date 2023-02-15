bc8511e89f07c3bd1efdfbf053eb0908c63cd8b7
krishna <krishnavm143@gmail.com>
2023-02-15 18:01:36 +0530

    commit

--- Backend/RestDemo/index.js
+++ Backend/RestDemo/index.js
@@ -1,11 +1,45 @@
 const express = require("express");
 const app = express();
+const path=require('path')
 var bodyParser = require("body-parser");
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 // parse application/json
 app.use(bodyParser.json());
+app.set('view engine','ejs')
+app.set('views',path.join(__dirname,'/views'))
+
+let comments=[
+  {
+    // id: uuid(),
+    username: 'Todd',
+    comment: 'lol that is so funny!'
+},
+{
+    // id: uuid(),
+    username: 'Skyler',
+    comment: 'I like to go birdwatching with my dog'
+},
+{
+    // id: uuid(),
+    username: 'Sk8erBoi',
+    comment: 'Plz delete your account, Todd'
+},
+{
+    // id: uuid(),
+    username: 'onlysayswoof',
+    comment: 'woof woof woof'
+}
+]
+
+app.get('/comments',(req,res)=>{
+  res.render('comments/index',{comments})
+})
+
+app.get('/comments/new',(req,res)=>{
+  res.render('comments/new')
+})
 
 app.get("/tacos", (req, res) => {
   res.json(req.body);
@@ -13,7 +47,8 @@ app.get("/tacos", (req, res) => {
 
 app.post("/tacos", (req, res) => {
   //   res.send("Get tacos");
-  res.json(req.body);
+  const {veg,qty}=req.body;
+  res.send(`OK ${veg} ${qty}`)
   console.log(req.body);
 });
 
